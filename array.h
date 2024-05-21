521d1b74eb0c33e7f6529a808ba1b6ed2fc5fecd
Marcino9696 <252884@edu.p.lodz.pl>
2024-05-21 21:55:25 +0200

    initial commit

---
+++ array.h
@@ -0,0 +1,18 @@
+#ifndef MYFIRSTMENU_ARRAY_H
+#define MYFIRSTMENU_ARRAY_H
+
+#define SIZE 10
+
+void printArray(int array[]);
+
+int findMaximumValue(int array[]);
+
+int findMinimumValue(int array[]);
+
+float calculateAverage(int array[]);
+
+void enterValuesIntoArray(int array[]);
+
+void displayResults(int min, int max, float average);
+
+#endif //MYFIRSTMENU_ARRAY_H
